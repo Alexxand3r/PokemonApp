eea4956858aa232d7f4e97a6e11a6ba1d8730cfd
Alexander De Simone <cristianoali08i@gmail.com>
2023-01-18 02:54:30 -0300

    test cards finalizado

---
+++ force-app/main/default/lwc/cardRegistros/cardRegistros.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+
+export default class CardRegistros extends LightningElement {
+  pokePrueba ={
+Name:"pikachu",
+Generacion:1,
+Imagen_URL__c:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
+Tipo__c:"Electric"
+  };
+}
